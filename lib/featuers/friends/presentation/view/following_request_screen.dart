b1a231db1253c773cbcd48453f4170b3084a86e2
mohamed-mmdouh156 <mmdouh112@gmail.com>
2025-04-21 22:58:41 +0200

    update 21/4/2025

--- lib/featuers/friends/presentation/view/following_request_screen.dart
+++ lib/featuers/friends/presentation/view/following_request_screen.dart
@@ -57,83 +57,88 @@ class _FollowingRequestScreenState extends State<FollowingRequestScreen> with Wi
           var cubit = context.read<FriendsCubit>();
           return ModalProgressHUD(
             inAsyncCall: state is AcceptFollowRequestLoadingState || state is RejectFollowRequestLoadingState,
-            child: SingleChildScrollView(
-              child: state is GetFollowingLoadingState? const Center(child: CircularProgressIndicator()): ListView.separated(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: cubit.followersRequest.length,
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 0,);
-                },
-                itemBuilder: (context, index) => Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.sizeOf(context).height*0.02,
-                      vertical: MediaQuery.sizeOf(context).height*0.01
-                  ),
-                  height:  MediaQuery.sizeOf(context).height*0.13,
-                  decoration: BoxDecoration(
-                    color: ColorManager.gray,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 25,
-                          child: CircleAvatar(
-                            radius: 23,
-                            child: Image(
-                              image: const AssetImage(AssetsManager.userIcon),
-                            ),),
-                        ),
-
-                        SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
-
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(cubit.followersRequest[index].userName,style:TextStyles.textStyle18Medium.copyWith(
-                                  color: ColorManager.black
-                              )),
-                              SizedBox(height: 5,),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: DefaultButton(
-                                      buttonText: AppLocalizations.of(context)!.translate('follow').toString(),
-                                      onPressed: () async {
-                                        cubit.acceptFollowRequest(
-                                            userId: UserDataFromStorage.uIdFromStorage,
-                                            followerId: cubit.followersRequest[index].userId
-                                        );
-                                      },
-                                      buttonColor: ColorManager.primaryBlue,
-                                    ),
-                                  ),
-                                  SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
-                                  Expanded(
-                                    child: DefaultButton(
-                                      buttonText: AppLocalizations.of(context)!.translate('decline').toString(),
-                                      onPressed: () async {
-                                        cubit.rejectFollowRequest(
-                                            userId: UserDataFromStorage.uIdFromStorage,
-                                            followerId: cubit.followersRequest[index].userId
-                                        );
-                                      },
-                                      buttonColor: ColorManager.error,
-                                    ),
-                                  ),
-                                  SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
-                                ],
-                              )
-                            ],
-                          ),
-                        )
-                      ]),
+            child: state is GetFollowingLoadingState? const Center(child: CircularProgressIndicator()):
+            cubit.followersRequest.isNotEmpty? ListView.separated(
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              itemCount: cubit.followersRequest.length,
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 0,);
+              },
+              itemBuilder: (context, index) => Container(
+                margin: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.sizeOf(context).height*0.02,
+                    vertical: MediaQuery.sizeOf(context).height*0.01
                 ),
+                height:  MediaQuery.sizeOf(context).height*0.13,
+                decoration: BoxDecoration(
+                  color: ColorManager.gray,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 25,
+                        child: CircleAvatar(
+                          radius: 23,
+                          child: Image(
+                            image: const AssetImage(AssetsManager.userIcon),
+                          ),),
+                      ),
+
+                      SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
+
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(cubit.followersRequest[index].userName,style:TextStyles.textStyle18Medium.copyWith(
+                                color: ColorManager.black
+                            )),
+                            SizedBox(height: 5,),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: DefaultButton(
+                                    buttonText: AppLocalizations.of(context)!.translate('follow').toString(),
+                                    onPressed: () async {
+                                      cubit.acceptFollowRequest(
+                                          userId: UserDataFromStorage.uIdFromStorage,
+                                          followerId: cubit.followersRequest[index].userId
+                                      );
+                                    },
+                                    buttonColor: ColorManager.primaryBlue,
+                                  ),
+                                ),
+                                SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
+                                Expanded(
+                                  child: DefaultButton(
+                                    buttonText: AppLocalizations.of(context)!.translate('decline').toString(),
+                                    onPressed: () async {
+                                      cubit.rejectFollowRequest(
+                                          userId: UserDataFromStorage.uIdFromStorage,
+                                          followerId: cubit.followersRequest[index].userId
+                                      );
+                                    },
+                                    buttonColor: ColorManager.error,
+                                  ),
+                                ),
+                                SizedBox(width: MediaQuery.sizeOf(context).height*0.02,),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ]),
               ),
+            ): Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(child: Text(AppLocalizations.of(context)!.translate('noFollowRequests').toString(),style: TextStyles.textStyle18Medium.copyWith(color: ColorManager.black),)),
+              ],
             ),
           );
         },
