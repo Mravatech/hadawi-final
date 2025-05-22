e04f727bfbcc7c0f32319bae8a9237c322a6e2bf
mohamed-mmdouh156 <mmdouh112@gmail.com>
2025-05-22 11:50:00 +0300

    upload version ios 22/5/2025

--- lib/widgets/default_app_bar_widget.dart
+++ lib/widgets/default_app_bar_widget.dart
@@ -6,9 +6,32 @@ import 'package:hadawi_app/styles/colors/color_manager.dart';
 import 'package:hadawi_app/styles/size_config/app_size_config.dart';
 import 'package:hadawi_app/styles/text_styles/text_styles.dart';
 
-AppBar defaultAppBarWidget({required String appBarTitle,required BuildContext context}) {
-  return AppBar(
-    leading: InkWell(
+AppBar defaultAppBarWidget({required String appBarTitle,required BuildContext context, bool isHomeLayout = false}) {
+  return isHomeLayout ? AppBar(
+    backgroundColor: ColorManager.gray,
+    titleSpacing: 0.0,
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        SizedBox(width:  SizeConfig.height*0.02,),
+        Text(
+          appBarTitle, style: TextStyles.textStyle18Bold.copyWith(
+            color: ColorManager.darkGrey,
+            fontSize: SizeConfig.height*0.023
+        ),),
+        Spacer(),
+        Image(
+            height: SizeConfig.height*0.05,
+            image: AssetImage(AssetsManager.logoWithoutBackground
+            )
+        ),
+
+        SizedBox(width:  SizeConfig.height*0.02,)
+
+      ],
+    ),
+  ) :AppBar(
+    leading:InkWell(
       onTap: () {
         print('fdf ${context.read<HomeCubit>().currentIndex}');
         Navigator.pop(context);
